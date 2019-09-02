a361a0d016f7d2be21ffd14aa66f021f35a44364
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-09-01 22:57:05 -0300

    Day2 - Domínio promoção

---
+++ src/main/java/br/com/lromagnoli/clonepelando/domain/Promocao.java
@@ -0,0 +1,52 @@
+package br.com.lromagnoli.clonepelando.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "promocoes")
+public class Promocao implements Serializable{
+
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "titulo", nullable = false)
+	private String titulo;
+	
+	@Column(name = "link_promocao", nullable = false)
+	private String linkPromocao;
+	
+	@Column(name = "site_promocao", nullable = false)
+	private String site;
+	
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@Column(name = "link_imagem", nullable = false)
+	private String linkImagem;
+	
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	@Column(name = "preco_promocao", nullable = false)
+	private BigDecimal preco;
+	
+	@Column(name = "total_likes")
+	private int likes;
+	
+	@Column(name = "data_cadastro", nullable = false)
+	private LocalDateTime dtCadastro;
+}
